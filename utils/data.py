ace1f977861c0075460de5ecc9cd4e62750e5451
DhanushkiMapitigama <dhanumapitigama@gmail.com>
2025-05-28 13:43:59 +0200

    dev: add data download script MNIST

---
+++ utils/data.py
@@ -0,0 +1,14 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_mnist_loaders(batch_size=64):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.view(-1)) # Flatten
+    ])
+    train_set = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    test_set = datasets.MNIST('./data', train=False, download=True, transform=transform)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+    
+    return train_loader, test_loader

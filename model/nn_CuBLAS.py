3e2cc254c5c25574e13f8685d422a69b11107eef
DhanushkiMapitigama <dhanumapitigama@gmail.com>
2025-08-15 14:08:37 +0200

    feature: add CuBLAS implementation

---
+++ model/nn_CuBLAS.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from model.cublas_linear import CuBLASLinear
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = CuBLASLinear(28*28, 64, "ReLU")
+        self.relu = nn.ReLU()
+        self.fc2 = CuBLASLinear(64, 10)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
